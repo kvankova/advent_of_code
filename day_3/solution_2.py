8ad2cb74a0afd669ce34f302e794b06ca46a10e4
Katerina Vankova <katerinavankova1@gmail.com>
2023-12-09 23:57:13 +0100

    d3s2

--- day_3/solution_2.py
+++ day_3/solution_2.py
@@ -1,36 +1,76 @@
 import re
-part_numbers = []
-asterisk_positions = {}
-digit_positions = {}
 
-def process_line(line):
-    digit_positions = []
-    asterisk_positions = []
-
-    for position, line_item in enumerate(line):
-        if line_item == '.':
-            continue
-        elif line_item.isdigit():
-            digit_positions.append(position)
-        elif line_item=="*":
-            asterisk_positions.append(position)
-    return digit_positions, asterisk_positions
-
-
-with open('puzzle.txt') as puzzle:
+with open('small_example_2.txt') as puzzle:
     lines = puzzle.readlines()
 
-    for line in range(len(lines)-2):
+gear_ratios = 0
+
+for line in range(len(lines)-2):
+    top_line = lines[line]
+    middle_line =  lines[line+1]
+    bottom_line = lines[line+2]
+
+    top_numbers = re.findall(r'\d+', top_line)
+    top_numbers_start_position = [(m.start(0), m.end(0)) for m in re.finditer(r'\d+', top_line)]
+    
+    top_line_numbers_positions = dict(zip(top_numbers, top_numbers_start_position))
+    
+    middle_numbers  = re.findall(r'\d+', middle_line)
+    middle_numbers_start_position = [(m.start(0), m.end(0)) for m in re.finditer(r'\d+', middle_line)]
+
+    middle_line_numbers_positions = dict(zip(middle_numbers, middle_numbers_start_position))
+
+    bottom_numbers = re.findall(r'\d+', bottom_line)
+    bottom_numbers_start_position = [(m.start(0), m.end(0)) for m in re.finditer(r'\d+', bottom_line)]
+    
+    bottom_line_numbers_positions = dict(zip(bottom_numbers, bottom_numbers_start_position))
+
+    middle_asterisks_positions = [m.start(0) for m in re.finditer(r'\*', middle_line)]
+
+
+    asterisk_numbers = {position:[] for position in middle_asterisks_positions}
+    for number in top_line_numbers_positions:
+        number_included = False
+        for position in range(top_line_numbers_positions[number][0], top_line_numbers_positions[number][1]):
+            if (position in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position].append(number)
+                number_included = True
+            elif (position-1 in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position-1].append(number)
+                number_included = True
+            elif (position+1 in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position+1].append(number)
+                number_included = True
+
+    for number in bottom_line_numbers_positions:
+        number_included = False
+        for position in range(bottom_line_numbers_positions[number][0], bottom_line_numbers_positions[number][1]):
+            if (position in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position].append(number)
+                number_included = True
+            elif (position-1 in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position-1].append(number)
+                number_included = True
+            elif (position+1 in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position+1].append(number)
+                number_included = True
         
-        top_line = lines[line]
-        middle_line = lines[line+1]
-        bottom_line = lines[line+2]
-        print(top_line, middle_line, bottom_line)
+    for number in middle_line_numbers_positions:
+        number_included = False
+        for position in range(middle_line_numbers_positions[number][0], middle_line_numbers_positions[number][1]):
+            if (position-1 in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position-1].append(number)
+                number_included = True
+            elif (position+1 in middle_asterisks_positions) and (not number_included):
+                asterisk_numbers[position+1].append(number)
+                number_included = True
 
-        part_numbers_positions = []
+    # if list's length is 2 then multiply numbers 
+    for asterisk in asterisk_numbers:
+        if len(asterisk_numbers[asterisk]) == 2:
+            print(asterisk_numbers[asterisk])
+            gear_ratios+= (int(asterisk_numbers[asterisk][0])*int(asterisk_numbers[asterisk][1]))
+    print(gear_ratios)
 
-        digit_positions[line], asterisk_positions[line] = process_line(top_line)
-        digit_positions[line+1], asterisk_positions[line+1] = process_line(middle_line)
-        digit_positions[line+2], asterisk_positions[line+2] = process_line(bottom_line)
 
-        
+    
